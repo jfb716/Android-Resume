c784a5b466d1a6cfa2eb12b6edd3e1aee2753757
John Black <john.f.black@icloud.com>
2016-08-06 20:04:04 -0400

    Initial Commit, started the heading and education section

---
+++ app/src/main/java/com/johnfblack/resume/MainActivity.java
@@ -0,0 +1,22 @@
+package com.johnfblack.resume;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
